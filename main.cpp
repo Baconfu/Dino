3a5cf6a59b34f7458a174c8c605c852e38bfb40d
Chuan-windows <11280@kristin.school.nz>
2020-11-18 14:49:34 +1300

    engine initial, do not pull yet

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <avatar.h>
 
 int main(int argc, char *argv[])
 {
+
+
+
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
